52b7fff568fdc1e6e7d3b6a73af56a39673535d0
appietro516 <appietro516@email.arizona.edu>
2020-08-31 02:57:00 -0700

    added contact emojis

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,10 +10,10 @@ const Contact = () => (
         <div className ="card-body">
             <h1 className ="text-center mb-4">Contact</h1>
             <p className = "mb-1">You can contact me at any of the following locations:</p>
-            <ul>
-                <li>Email: <a href="mailto:appietro516@email.arizona.edu">appietro516@email.arizona.edu</a></li>
-                <li>Call or Text: (520) 329-7251</li>
-                <li>Discord: Appietro516#7309</li>
+            <ul className = "custom-bullets">
+                <li className ="email-icon">Email: <a href="mailto:appietro516@email.arizona.edu">appietro516@email.arizona.edu</a></li>
+                <li className ="phone-icon">Call or Text: (520) 329-7251</li>
+                <li className ="discord-icon">Discord: Appietro516#7309</li>
             </ul>
         </div>
     </div>
